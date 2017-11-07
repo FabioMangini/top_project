295ea9bc6aef3f600eeb4bd0b59c11824f569acc
FabioMangini <fabio.mangini@nersc.no>
2017-11-07 20:35:59 +0000

    modified: model.py, setting.py

--- top_project/settings.py
+++ top_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [u'192.168.33.10',]
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
